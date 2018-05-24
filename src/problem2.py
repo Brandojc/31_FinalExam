d5ada407d61d54ec9916edc211c461e5de12028f
brandojc <brandojc@rose-hulman.edu>
2018-05-24 14:29:28 -0400

    Done

--- src/problem2.py
+++ src/problem2.py
@@ -2,9 +2,9 @@
 Final exam, problem 2.
 
 Authors: David Mutchler, Dave Fisher, Matt Boutell, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.  May 2018.
+         their colleagues and James Brandon.  May 2018.
 
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+"""  # Done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import time
 import testing_helper
@@ -19,8 +19,8 @@ def main():
     print()
     print('Un-comment the calls in MAIN one by one')
     print(' to run the testing code as you complete the TODOs.')
-    # run_test_problem2a()
-    # run_test_problem2b()
+    run_test_problem2a()
+    run_test_problem2b()
     # run_test_problem2c()
 
 
@@ -170,10 +170,15 @@ def problem2a(x, sequence):
       :type sequence:  [int]
     """
     # -------------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # done: 2. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
+    seq_1 = []
 
+    for k in range(len(sequence)):
+        if sequence[k] > x:
+            seq_1 = seq_1 + [sequence[k]]
+    return seq_1
 
 def run_test_problem2b():
     """ Tests the  problem2b   function. """
@@ -253,6 +258,10 @@ def problem2b(sequence):
     # TODO: 3. Implement and test this function.
     #          Tests have been written for you (above).
     # -------------------------------------------------------------------------
+    for k in range(len(sequence)):
+        for j in range(len(sequence)):
+            if abs(sequence[k]) > abs(sequence[j]):
+                return k
 
 
 def run_test_problem2c():
